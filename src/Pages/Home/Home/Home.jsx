204d15887b7ca15e2a595c64706f97e344fccb58
muhammad <programalltest@gmail.com>
2023-05-18 16:40:03 +0600

    Shop by category section done

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../../shared/Header/Header";
 import Gallery from "../Gallery/Gallery";
+import Products from "../Products/Products";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Gallery />
+      <Products />
     </div>
   );
 };
---
+++ src/Pages/Home/Products/Products.jsx
@@ -0,0 +1,113 @@
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+import { FaStar } from "react-icons/fa";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+const Products = () => {
+  const [activeTab, setActiveTab] = useState(0);
+  const [toys, setToys] = useState([]);
+  useEffect(() => {
+    let category;
+    if (activeTab === 0) {
+      category = "Math_Learning_Toys";
+    } else if (activeTab === 1) {
+      category = "Engineering_Toys";
+    } else {
+      category = "Language_Toys";
+    }
+    fetch(`http://localhost:5000/toys/${category}`)
+      .then((res) => res.json())
+      .then((data) => setToys(data));
+  }, [activeTab]);
+
+  return (
+    <div className="w-full p-5 bg-gray-100">
+      <Tabs forceRenderTabPanel defaultIndex={1}>
+        <TabList>
+          <Tab>Educational and learning toys</Tab>
+        </TabList>
+        <TabPanel>
+          <Tabs
+            forceRenderTabPanel
+            onSelect={(e) => {
+              setActiveTab(e);
+            }}
+          >
+            <TabList>
+              <Tab>Math Learning Toys</Tab>
+              <Tab>Engineering Toys</Tab>
+              <Tab>Language Toys</Tab>
+            </TabList>
+            <TabPanel>
+              <div className="flex flex-wrap gap-2 justify-center">
+                {toys.map((toy) => (
+                  <Card
+                    key={toy?._id}
+                    image={toy?.toy_imageUrl}
+                    price={toy?.price}
+                    name={toy?.toyname}
+                    rating={toy?.rating}
+                    description={toy?.description}
+                  />
+                ))}
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="flex flex-wrap gap-2 justify-center">
+                {toys.map((toy) => (
+                  <Card
+                    key={toy?._id}
+                    image={toy?.toy_imageUrl}
+                    price={toy?.price}
+                    name={toy?.toyname}
+                    rating={toy?.rating}
+                    description={toy?.description}
+                  />
+                ))}
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="flex flex-wrap gap-2 justify-center">
+                {toys.map((toy) => (
+                  <Card
+                    key={toy?._id}
+                    image={toy?.toy_imageUrl}
+                    price={toy?.price}
+                    name={toy?.toyname}
+                    rating={toy?.rating}
+                    description={toy?.description}
+                  />
+                ))}
+              </div>
+            </TabPanel>
+          </Tabs>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
+};
+
+const Card = ({ image, name, price, rating, description }) => {
+  const notify = () => toast("here is tost");
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl">
+      <figure>
+        <img className="h-64" src={image} alt={name} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{name}</h2>
+        <p>{description}</p>
+        <h1>Price : ${price}</h1>
+        <div className="flex items-center justify-between">
+          <button onClick={notify} className="btn btn-primary">
+            view details
+          </button>
+          <div className="btn text-xl">
+            <FaStar /> {rating}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Products;
